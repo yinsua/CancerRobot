d104d473169d7e76732a45411ae25790e08640bb
yinsua <ysh.001@qq.com>
2015-08-12 11:44:56 +0800

    basic ok

--- Protocol/Communicate.cs
+++ Protocol/Communicate.cs
@@ -6,68 +6,172 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using System.IO.Ports;
 
 namespace communicate
 {
     class Communicate
     {       
+        
+        //通信方式
+        //  0 - Udp
+        //  1 - Serial
+        public static int type = 0;
+        
+        //定义串口
+        SerialPort sp = new SerialPort();
+        public static string strPortName = "COM1";
+        public static string strBaudRate = "9600";
+        public static string strDataBits = "8";
+        public static string strStopBits = "1";
+        
         //用于接收        
         public static Socket recvSocket;
         
         //用于发送
         public static EndPoint sendSocket;
         
-        public void Communicate(string ip,int sendPort,int recvPort){
-            IPEndPoint no = new IPEndPoint(IPAddress.Parse(ip), sendPort);
-            sendSocket = (EndPoint)(no);
-            sendSocket.SendTimeout = 3000;
+        public void Communicate(string arg_1,int arg_2,int arg_3)
+        {
+            if(type==1)
+            {                              
+                strPortName = arg_1;
+                strBaudRate = Convert.toString(arg_2);
+                strDataBits = Convert.toString(arg_3);
+                  
+                 //定义串口                
+                sp.PortName = strPortName;
+                sp.BaudRate = int.Parse(strBaudRate);
+                sp.DataBits = int.Parse(strDataBits);
+                sp.StopBits = (StopBits)int.Parse(strStopBits);
+                sp.ReadTimeout = 1000;
+                sp.SendTimeout = 1000;
+                      
+                //打开串口
+                sp.Open();                 
+            }
+            else if(type==0)
+            {
+                ip = arg_1;
+                sendPort = arg_2;
+                recvPort = arg_3;
+                
+                IPEndPoint no = new IPEndPoint(IPAddress.Parse(ip), sendPort);
+                sendSocket = (EndPoint)(no);
+                sendSocket.SendTimeout = 3000;
+                
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, recvPort);
+                recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                recvSocket.ReceiveTimeout = 3000;    
+            }
+  
             
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, recvPort);
-            recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            recvSocket.ReceiveTimeout = 3000;         
+    
         }
         
         public static void sendData(string data)
-        {               
-            byte[] sendDataString = new byte[1024];            
-            sendDataString = Encoding.ASCII.GetBytes(data);
-            try
+        {
+            //串口方式
+            if (type==1)
             {
-                sendSocket.SendTo(sendDataString, sendDataString.Length, SocketFlags.None, sendSocket);
-            }
-            catch (Exception ex)
+                try
+                {
+                    //第一种发送接收方式
+                    sp.Write(data);
+                    //string strRecieve1 = sp.ReadExisting();
+                   
+                    ////第二种发送接收方式
+                    //sp.WriteLine(data);
+                    //string strRecieve = sp.ReadLine();
+         
+                    ////第三种发送接收方式
+                    //byte[] SendBuf = new byte[256];
+                    //SendBuf = System.Text.Encoding.UTF8.GetBytes(data);
+                    //int len = data.Length;
+                    //sp.Write(SendBuf, 0, len);
+         
+                    //byte[] RecieveBuf = new byte[256];
+                    //sp.Read(RecieveBuf, 0, 256);
+                    //string strRecieve = System.Text.Encoding.UTF8.GetString(RecieveBuf);
+                    //txtRecieve.Text = strRecieve3;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("发送失败！\n" + ex.Data); 
+                }
+
+            }   
+            //UDP方式
+            else if(type==0)
             {
-                MessageBox.Show("发送失败！\n" + ex.Data); 
+                byte[] sendDataString = new byte[1024];            
+                sendDataString = Encoding.ASCII.GetBytes(data);
+                try
+                {
+                    sendSocket.SendTo(sendDataString, sendDataString.Length, SocketFlags.None, sendSocket);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("发送失败！\n" + ex.Data); 
+                }
             }
+            else
+            {
+                MessageBox.Show("type error!");
+            }            
+
         }
             
         public static string recvData()
         {
-            int recv;
-            string recvTemp = null;
-            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-            EndPoint Remote = (EndPoint)(sender);
-            byte[] data = new byte[1024];
-            
-            try
+            //串口方式
+            if (type==1)
             {
-                recv = recvSocket.ReceiveFrom(data, ref Remote);
+                try
+                {
+                    string strRecieve = sp.ReadExisting();
+                    return strRecieve;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("接收失败！\n" + ex.Data);
+                    return "-1";
+                }
             }
-            catch (Exception ex)
+            //UDP方式
+            else if (type==0)
             {
-                MessageBox.Show("接收失败！\n" + ex.Data)
-            }
-            
-            if (recv > 0)
-            {
-                recvTemp = Encoding.ASCII.GetString(data, 0, recv);
-                return recvTemp;
+                int recv;
+                string recvTemp = null;
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint Remote = (EndPoint)(sender);
+                byte[] data = new byte[1024];
+                
+                try
+                {
+                    recv = recvSocket.ReceiveFrom(data, ref Remote);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("接收失败！\n" + ex.Data)
+                    return "-1";
+                }
+                
+                if (recv > 0)
+                {
+                    recvTemp = Encoding.ASCII.GetString(data, 0, recv);
+                    return recvTemp;
+                }
+                else
+                {
+                    return "-1";
+                }
             }
             else
             {
-                return "-1";
-            }
-            
+                MessageBox.Show("type error!");
+            }            
         }
         
         private static String convertToGcodeJSON(string data)
@@ -127,6 +231,18 @@ namespace communicate
                 //MessageBox.Show("Read File Error! " + ex.Data);
                 return;
             }
+        }        
+        
+        public static void closeSerial()
+        {
+            sp.Close()            
+        }
+        
+        public static void closeSocket()
+        {
+            //关闭套接字
+            recvSocket.Close();
+            sendSocket.Close();
         }
     }
 }
--- Protocol/README.md
+++ Protocol/README.md
@@ -10,12 +10,15 @@
 ## `下位机IP及端口`
 ## Arduino 192.168.1.106  8023
 
-1. 引用communicate空间
-2. 创建Communicate类
-3. 调用类方法
 
 ###API说明
 * 创建类时三个参数分别是目标IP、目标端口、本地端口
 * 发送Gcode：sendGcode(Gcode)  (注：参数Gcode是类似这样的`字符串`："G01 X-20 Y30 Z60")
 * 设置寄存器：WriteReg(regNumber,status)  (注：regNumber是寄存器编号`数字`，status是要设置的状态0或者1的`数字`)
 * 读寄存器：ReadReg(regNumber)  (注：regNumber是寄存器编号`数字`)
+
+###使用说明
+1. 引用communicate空间
+2. 创建Communicate类 （传入对应参数 ）
+3. 设置通信类型
+4. 调用类方法
